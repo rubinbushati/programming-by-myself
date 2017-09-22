b95a7ad3b863f39e8349dd618a341127264f8463
Rubin Bushati <rubin.bushati12@gmail.com>
2017-09-22 00:15:25 -0700

    Update main.pl

--- main.pl
+++ main.pl
@@ -2,13 +2,35 @@ use warnings;
 use diagnostics;
 use strict;
 
-#added another commanduse feature 'say';
+#added another command that i can write a line of words using the command say :)
+use feature 'say';
 
 
-my %things = ["Study", "Working"];
+
+my %things = ["Study", "Working"];
 my $remember_things = "Family";
 
-print "\$things{'Study'} = $things{'Study'} But my important things are $remember_things\n";
 
-
+print "\$things{'Study'} = $things{'Study'} But my important things are $remember_things\n";
+
+print "whats your password ";
+my $password = <STDIN>;
+
+if($password == 1872){
+    print"Welcome sir\n";
+}else{
+  print"The password may be Invalid!\n";
+}
+
+my $access = <STIDN>;
+print"Put your finger on your right: ";
+
+if($access = true){
+  print" Access Gratuated!\n";
+}else{
+  print"Access Denied!\n";
+}
+
+
+
 
