b270a3096cdb43417e71f2111ec8ef8864fae568
Rubin Bushati <rubin.bushati12@gmail.com>
2017-09-24 07:39:54 -0700

    this file has been updated
    This a way how you can send an email through Perl Programming but you should need the ip of the server where the email should be sendet and the ip of the server of the email account!

--- main.pl
+++ main.pl
@@ -1,35 +1,21 @@
-use warnings;
-use diagnostics;
-use strict;
+use MIME::Lite;
+ 
+$to = 'abcd@gmail.com';
+$cc = 'efgh@mail.com';
+$from = 'webmaster@yourdomain.com';
+$subject = 'Test Email';
+$message = 'This is test email sent by Perl Script';
 
-#added another command that i can write a line of words using the command say :)
-use feature 'say';
-
-
-
-my %things = ["Study", "Working"];
-my $remember_things = "Family";
-
-
-print "\$things{'Study'} = $things{'Study'} But my important things are $remember_things\n";
-
-print "whats your password ";
-my $password = <STDIN>;
-
-if($password == 1872){
-    print"Welcome sir\n";
-}else{
-  print"The password may be Invalid!\n";
-}
-
-my $access = <STIDN>;
-print"Put your finger on your right: ";
-
-if($access = true){
-  print" Access Gratuated!\n";
-}else{
-  print"Access Denied!\n";
-}
+$msg = MIME::Lite->new(
+                 From     => $from,
+                 To       => $to,
+                 Cc       => $cc,
+                 Subject  => $subject,
+                 Data     => $message
+                 );
+                 
+$msg->send;
+print "Email Sent Successfully\n";
 
 
 
